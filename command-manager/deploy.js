5bbf7a918ffb59ae5db6e2910f7724602d08192b
Troy C. <327529+troxor@users.noreply.github.com>
2025-01-10 16:52:30 -0600

    Fix command deployment, using dotenv

--- command-manager/deploy.js
+++ command-manager/deploy.js
@@ -1,52 +1,38 @@
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
+require("dotenv").config();
 const fs = require("node:fs");
-const path = require("node:path");
-const { getClientData } = require("../src/utils/getClientData");
+// const { getClientData } = require("../src/utils/getClientData");
 
-const run = async () => {
-  require("dotenv").config();
-  const clientData = await getClientData();
-  const clientId = process.env.DISCORD_CLIENT_ID;
-  const token = process.env.DISCORD_TOKEN;
-  const commands = [];
-  // Grab all the command folders from the commands directory you created earlier
-  const folderPath = path.join(__dirname, "../src/Commands");
+// const clientData = await getClientData();
+const guildId = process.env.DISCORD_GUILD_ID;
 
-  const commandFiles = fs
-    .readdirSync(folderPath)
-    .filter((file) => file.endsWith(".js"));
-  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-  for (const file of commandFiles) {
-    const filePath = path.join(folderPath, file);
-    const command = require(filePath);
-    commands.push(command.builder);
+const commands = [];
+const commandFiles = fs.readdirSync(`${__dirname}/../src/Commands`).filter((file) => file.endsWith("auction.js"));
+
+for (const file of commandFiles) {
+  const command = require(`${__dirname}/../src/Commands/${file}`);
+  commands.push(command.builder);
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID),
+      { body: commands },
+    );
+
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
   }
+})();
 
-  // Construct and prepare an instance of the REST module
-  const rest = new REST().setToken(token);
-
-  // and deploy your commands!
-  (async () => {
-    try {
-      console.log(
-        `Started refreshing ${commands.length} application (/) commands.`
-      );
-
-      // The put method is used to fully refresh all commands in the guild with the current set
-      const data = await rest.put(Routes.applicationCommands(clientId), {
-        body: commands,
-      });
-
-      console.log(
-        `Successfully reloaded ${data.length} application (/) commands.`
-      );
-    } catch (error) {
-      // And of course, make sure you catch and log any errors!
-      console.error(error);
-    }
-  })();
-};
-
-run();
 // vim: tabstop=2 expandtab shiftwidth=2 sts=2
