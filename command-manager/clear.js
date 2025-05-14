59db46f9b7e789fd1c36123d2508b0174c252cb4
Troy C. <327529+troxor@users.noreply.github.com>
2025-05-14 16:29:02 -0500

    feat: Adapt to use env vars

--- command-manager/clear.js
+++ command-manager/clear.js
@@ -1,20 +1,15 @@
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const { getClientData } = require("../src/utils/getClientData");
+require("dotenv").config();
 
-const guildID = "805802838630203433";
-// ...
-const run = async () => {
-  const clientData = await getClientData();
-  const rest = new REST({ version: "9" }).setToken(clientData.token);
-  rest
-    .put(Routes.applicationGuildCommands(clientData.id, guildID), {
-      body: [],
-    })
-    .then(() =>
-      console.log("Successfully deleted all application guild commands.")
-    )
-    .catch(console.error);
-};
-
-run();
+(async () => {
+	const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN);
+	rest
+		.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID), {
+			body: [],
+		})
+		.then(() =>
+			console.log("Successfully deleted all application guild commands.")
+		)
+		.catch(console.error);
+})();
